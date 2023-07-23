5497d26674fda22f342a010ad62362ab85a93423
RLO5 <114177705+RLO5@users.noreply.github.com>
2023-07-23 23:33:41 +0330

    Update index.js
    Update Contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x4b53c5d0aaf988bfe6d084ded8f498e0ff7fe88c";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
